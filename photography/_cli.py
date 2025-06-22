f3499c9b078e3b2e27a0730b88b891526e9793ff
Julian Berman <Julian@GrayVines.com>
2025-06-22 15:44:21 -0400

    Try to start untangling...

--- photography/_cli.py
+++ photography/_cli.py
@@ -33,19 +33,18 @@ To Do:
 from __future__ import annotations
 
 from datetime import UTC, date, datetime, timedelta
-from functools import cached_property
 from pathlib import Path
 from time import strptime
 from typing import Any, Protocol
 from uuid import UUID
 import os
-import subprocess
 
 from attrs import field, frozen
 from PIL import ExifTags, Image
 from rpds import HashTrieMap
-import imagehash
 import rich_click as click
+
+from photography._media import Media, Photo, Video
 
 DESKTOP = (
     Path(os.environ["XDG_DESKTOP_DIR"])
@@ -276,76 +275,6 @@ def decide(path: Path) -> Effect:
             raise WTF(path, f"We haven't yet handled {extension} files.")
 
     return Import.if_dates_match(path=path, media=media)
-
-
-class Media(Protocol):
-    """
-    A photo or video.
-    """
-
-    hash: imagehash.ImageHash | None
-    metadata_datetime: datetime | None
-
-    @classmethod
-    def from_path(cls, path: Path):
-        """
-        Parse the media at the given path.
-        """
-
-
-@frozen
-class Photo:
-    """
-    A photo.
-    """
-
-    _exif: dict[ExifTags, Any]
-    #: `imagehash` doesn't cover videos and it's not often I have a cropped or
-    #: modified video which isn't otherwise easy to identify via e.g. tilde
-    #: naming, but maybe at some point we'll want some video hash
-    hash: imagehash.ImageHash
-
-    @cached_property
-    def metadata_datetime(self) -> datetime | None:
-        exif_date = self._exif.get(ExifTags.Base.DateTimeOriginal)
-        if exif_date is not None:
-            return datetime.fromisoformat(exif_date)
-
-    @classmethod
-    def from_path(cls, path: Path):
-        with Image.open(path) as image:
-            return cls(exif=image.getexif(), hash=imagehash.phash(image))
-
-
-@frozen
-class Video:
-    """
-    A video.
-    """
-
-    metadata_datetime: datetime | None
-    hash = None
-
-    @classmethod
-    def from_path(cls, path: Path):
-        stdout = subprocess.check_output(  # noqa: S603
-            [  # noqa: S607
-                "ffprobe",
-                "-v",
-                "quiet",
-                "-show_entries",
-                "format_tags=creation_time",
-                "-of",
-                "default=noprint_wrappers=1:nokey=1",
-                path,
-            ],
-            text=True,
-        )
-        return cls(
-            metadata_datetime=(
-                datetime.fromisoformat(stdout.strip()) if stdout else None
-            ),
-        )
 
 
 class Effect(Protocol):
